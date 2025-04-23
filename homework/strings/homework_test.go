18a4ae5ce32469f563540c4703e804c7a8d3af21
Oksana Egorova <teeara1991@yandex.ru>
2025-04-23 15:24:16 +0500

    hw3

--- homework/strings/homework_test.go
+++ homework/strings/homework_test.go
@@ -8,30 +8,56 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// go test -v homework_test.go
+
 type COWBuffer struct {
 	data []byte
 	refs *int
-	// need to implement
 }
 
 func NewCOWBuffer(data []byte) COWBuffer {
-	return COWBuffer{} // need to implement
+	return COWBuffer{
+		data: data,
+		refs: new(int),
+	}
 }
 
 func (b *COWBuffer) Clone() COWBuffer {
-	return COWBuffer{} // need to implement
+	(*b.refs)++
+	return COWBuffer{
+		data: b.data,
+		refs: b.refs,
+	}
 }
 
 func (b *COWBuffer) Close() {
-	// need to implement
+	if b.refs != nil && *b.refs > 0 {
+		*b.refs--
+	}
+	b.data = nil
+	b.refs = nil
 }
 
 func (b *COWBuffer) Update(index int, value byte) bool {
-	return false // need to implement
+	if index < 0 || index >= len(b.data) {
+		return false
+	}
+	if *b.refs > 1 {
+		b.data = append([]byte(nil), b.data...)
+		*b.refs--
+		b.refs = new(int)
+	}
+
+	b.data[index] = value
+	return true
 }
 
 func (b *COWBuffer) String() string {
-	return "" // need to implement
+	if len(b.data) == 0 {
+		return ""
+	}
+
+	return unsafe.String(unsafe.SliceData(b.data), len(b.data))
 }
 
 func TestCOWBuffer(t *testing.T) {
